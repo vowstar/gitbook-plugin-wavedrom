78c34779c4543b9dac67d9b23f3ad133e4b22c89
Huang Rui <vowstar@gmail.com>
2018-03-11 00:39:15 +0800

    Add test case for markdown parameter

--- test/index.js
+++ test/index.js
@@ -24,6 +24,24 @@ describe('wavedrom', function() {
                 assert.equal(svg.includes('clk'), true);
             });
     });
+    it('should correctly replace by ```wavedrom { width = 600, height = 800, foo = "bar" }``` tag', function() {
+        return tester.builder()
+            .withContent('\n```wavedrom { width = 600, height = 800, foo = "bar" }\n{ signal : [{ name: "clk",  wave: "p." }]} \n```')
+            .withLocalPlugin(path.join(__dirname, '..'))
+            .withBookJson({
+                gitbook: pkg.engines.gitbook,
+                plugins: ['wavedrom']
+            })
+            .create()
+            .then(function(result) {
+                const isSvg = require('is-svg');
+                const svg = result[0].content.match(/<svg[^]*<\/svg>/m).toString();
+
+                assert.equal(isSvg(svg), true);
+                assert.equal(svg.includes('path'), true);
+                assert.equal(svg.includes('clk'), true);
+            });
+    });
     it('should correctly replace by {% wavedrom %} and endwavedrom {% endwavedrom %} tag', function() {
         return tester.builder()
             .withContent('\n{% wavedrom %}\n{ signal : [{ name: "clk",  wave: "p." }]} \n{% endwavedrom %}')
